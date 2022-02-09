1374c1f65780762e80e663fbcd90c4fcbebf0e21
LiquidHider <selemetowq@gmail.com>
2022-02-10 01:18:31 +0200

    Replace Email credentials to appsettings.json.

--- HneuConferenсe/HneuConferenсe/EmailCredentials.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HneuConferenсe
-{
-    public class EmailCredentials
-    {
-        public string Email = "hneuconference@gmail.com";
-        public string Password = "trelloteam";
-    }
-}
