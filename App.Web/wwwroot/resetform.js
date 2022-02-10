d45cee5d261c8a9820cb342eb0e56a29583ef6a7
VladMak11 <blakcwolf2017@gmail.com>
2022-02-10 14:00:21 +0200

    Add reset roms js

---
+++ App.Web/wwwroot/resetform.js
@@ -0,0 +1,10 @@
+function clearText(){
+document.getElementById('PNO').value = "";
+document.getElementById('ND').value = "";
+document.getElementById('FullNameReporter').value = "";
+document.getElementById('Section').value = "";
+document.getElementById('Address').value = "";
+document.getElementById('Phone').value = "";
+document.getElementById('E-mail').value = "";
+document.getElementById('FullNameDirector').value = "";
+}
